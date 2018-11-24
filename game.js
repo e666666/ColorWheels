37ebd7cdc7ac0d7bdf645f558ee8590cc3bbf9c5
Alex Henderson <bohroco@aol.com>
2018-11-24 17:07:34 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,23 @@
+var blueTick = 0;
+var blueMax = 25;
+var totPower = 0;
+var blueMult = 1;
 
+function update(get, set) {
+  document.getElementById(get).innerHTML=set;
+}
+
+function blueClick() {
+  blueTick++
+}
+
+function gameLoop() {
+  if(blueTick>=blueMax) {
+    totPower+=blueMult;
+    blueTick=0;
+  }
+  update("powerAmount", totPower);
+  update("blueCircle1", ""+blueTick+"/"+blueMax);
+}
+
+setInterval(gameLoop(), 50); 
