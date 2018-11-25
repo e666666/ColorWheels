92f6e0bc181301d413d48024eaea593c750371e4
Alex Henderson <bohroco@aol.com>
2018-11-25 07:11:17 +0100

    Update game.js

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ var blueTick = 0;
 var blueMax = 25;
 var totPower = 0;
 var blueMult = 1;
+var upgrade1Price = 10;
 
 function update(get, set) {
   document.getElementById(get).innerHTML=set;
@@ -15,5 +16,13 @@ function blueClick() {
     blueTick=0;
     update("powerAmount", totPower);
     update("blueCircle1", "0/"+blueMax);
+  }
+}
+
+function checkUpgrade1() {
+  if(totPower>upgrade1Price){
+    blueMult++;
+    upgrade1Price*=2.5;
+    update("upgrade1", upgrade1Price);
   }
 }
