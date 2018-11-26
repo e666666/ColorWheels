773a878797a1f0f0fd77d0875fee4a53c570e5dc
Alex Henderson <bohroco@aol.com>
2018-11-26 07:02:58 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 var blueTick = 0;
 var blueMax = 25;
 var totPower = 0;
-var blueMult = 1;
-var upgrade1Price = 10;
+var blueMults = [1];
+var upgrade1Price = [10];
+var addBluePrice = 100; 
+var blueIndex = 0;
 
 function update(get, set) {
   document.getElementById(get).innerHTML=set;
@@ -10,21 +12,40 @@ function update(get, set) {
 
 function blueClick() {
   blueTick++;
-  update("blueCircle1", ""+blueTick+"/"+blueMax);
+  update("blueCircle", ""+blueTick+"/"+blueMax);
+  var mult=1;
+  for(var i=0;i<blueMults.length();i++){
+    mult*=blueMults[i];
+  }
   if(blueTick>=blueMax) {
-    totPower+=blueMult;
+    totPower+=mult;
     blueTick=0;
     update("powerAmount", totPower);
-    update("blueCircle1", "0/"+blueMax);
+    update("blueCircle", "0/"+blueMax);
   }
 }
 
-function checkUpgrade1() {
-  if(totPower>upgrade1Price){
-    blueMult++;
-    upgrade1Price*=2.5;
-    update("upgrade1", "Cost: "+upgrade1Price+" Power");
-    totPower-=10;
+function checkUpgrade1(num) {
+  var price=upgrade1Price[num-1];
+  if(totPower>price){
+    totPower-=price;
     update("powerAmount", totPower);
+    blueMults[num-1]=blueMults[num-1]+1;
+    price*=2.5
+    upgrade1Price[num-1]=price;
+    update("upgrade1", "Upgrade your Blue Button<br/>Cost: "+price+" Power");
+  }
+}
+
+function checkAddBlue() {
+  if(totPower>addBluePrice){
+    totPower-=price;
+    update("powerAmount", totPower);
+    if(blueIndex<9){
+      blueIndex++;
+      document.getElementById("blueButton"+blueIndex).style.visibility="visible";
+    }
+    blueMults.push(1);
+    upgrade1Price.push(100*blueIndex*blueIndex);
   }
 }
