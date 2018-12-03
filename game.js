ed09e761d3dd9035369ab763cff8745ed04620d2
Alex Henderson <bohroco@aol.com>
2018-12-03 20:56:06 +0100

    Update game.js

--- game.js
+++ game.js
@@ -11,16 +11,17 @@ function update(get, set) {
 }
 
 function blueClick() {
-  alert("yep");
   blueTick++;
   update("blueCycle", ""+blueTick+"/"+blueMax);
   var mult=1;
   if(blueTick>=blueMax) {
     blueMults.forEach(getMult);
     function getMult(value){
-      mult=bigMult(mult,value,1);
+      //mult=bigMult(mult,value,1);
+      mult*=value;
     }
-    totPower=bigAdd(totPower,mult,1);
+    //totPower=bigAdd(totPower,mult,1);
+    totPower+=mult;
     blueTick=0;
     update("powerAmount", totPower);
     update("blueCycle", "0/"+blueMax);
@@ -29,27 +30,34 @@ function blueClick() {
 
 function checkUpgrade1(num) {
   var price=upgrade1Price[num-1];
-  if(bigBigger(totPower,price)){
-    bigAdd(totPower,price,0);
+  //if(bigBigger(totPower,price)){
+  if(totPower>price){
+    //bigAdd(totPower,price,0);
+    totPower-=price;
     update("powerAmount", totPower);
     blueMults[num-1]=blueMults[num-1]+1;
     var name = "blueCircle" + num;
     update(name, "x"+blueMults[num-1]);
-    price=bigMult(price,2.5,1);
+    //price=bigMult(price,2.5,1);
+    price=Math.floor(price*2.5);
     upgrade1Price[num-1]=price;
     update("upgrade"+num, "Upgrade your Blue Button<br/>Cost: "+price+" Power");
   }
 }
 
 function checkAddBlue() {
-  if(bigBigger(totPower,addBlueprice)){
-    bigAdd(totPower,addBluePrice,0);
+  //if(bigBigger(totPower,addBlueprice)){
+  if(totPower>addBluePrice){
+    //bigAdd(totPower,addBluePrice,0);
+    totPower-=addBluePrice;
     blueIndex++;
     update("powerAmount", totPower);
     document.getElementById("buttonSet"+blueIndex).style.display="block";
     blueMults.push(1);
-    upgrade1Price.push("1e"+blueIndex);
-    bigMult(addBluePrice,10,1);
+    //upgrade1Price.push("1e"+blueIndex);
+    upgrade1Price.push(Math.pow(10,blueIndex));
+    //bigMult(addBluePrice,10,1);
+    addBluePrice*=10;
     update("addBlueButton", "Add another Blue Button<br/>Cost: "+addBluePrice+" Power");
   }
 }
