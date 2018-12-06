c2e0f6be1485552f72f7330c6e9031b6b904a886
Alex Henderson <bohroco@aol.com>
2018-12-06 06:21:55 +0100

    Testing save function

--- game.js
+++ game.js
@@ -1,10 +1,17 @@
-var blueTick = 0;
-var blueMax = 1000;
-var totPower = 0;
-var blueMults = [1];
-var upgrade1Price = [10];
-var addBluePrice = 100; 
-var blueIndex = 1;
+function getDefaultSave(){
+  return{ 
+    blueTick: 0,
+    blueMax: 1000,
+    totPower: 0,
+    blueMults: [1],
+    upgrade1Price: [10],
+    addBluePrice: 100,
+    blueIndex: 1,
+    lastTick: new Date.getTime(),
+  };
+}
+
+var user = getDefaultSave();
 
 function update(get, set) {
   document.getElementById(get).innerHTML=set;
@@ -18,13 +25,13 @@ function testStuff(){
 }
 
 function gameCycle(){
-  if(blueTick<1000){
-    blueTick+=10;
-    update("blueCycle", "Reset Cycle: "+blueTick+"/"+blueMax);
-    changeButtonOpacity(blueTick/10);
+  if(user.blueTick<1000){
+    user.blueTick+=10;
+    update("blueCycle", "Reset Cycle: "+user.blueTick+"/"+user.blueMax);
+    changeButtonOpacity(user.blueTick/10);
   }
-  for(i=0;i<upgrade1Price.length;i++){
-    if(bigBigger(totPower,upgrade1Price[i])){
+  for(i=0;i<user.upgrade1Price.length;i++){
+    if(bigBigger(user.totPower,user.upgrade1Price[i])){
       var j = i+1;
       document.getElementById("upgrade"+j).style.opacity = 1.0;
     }
@@ -33,24 +40,24 @@ function gameCycle(){
       document.getElementById("upgrade"+j).style.opacity = 0.6;
     }
   }
-  if(bigBigger(totPower,addBluePrice)) document.getElementById("addBlueButton").style.opacity = 1.0;
+  if(bigBigger(user.totPower,user.addBluePrice)) document.getElementById("addBlueButton").style.opacity = 1.0;
   else document.getElementById("addBlueButton").style.opacity = 0.6;
 }
 
 function blueClick() {
   var mult=1;
-  if(blueTick>=blueMax) {
-    blueMults.forEach(getMult);
+  if(user.blueTick>=user.blueMax) {
+    user.blueMults.forEach(getMult);
     function getMult(value){
       mult=bigMult(mult,value,1);
       //mult*=value;
     }
-    totPower=bigAdd(totPower,mult,1);
+    user.totPower=bigAdd(user.totPower,mult,1);
     //totPower+=mult;
-    blueTick=0;
-    update("powerAmount", "Total Power: "+display(totPower));
-    blueTick=0;
-    update("blueCycle", "Reset Cycle: 0/"+blueMax);
+    user.blueTick=0;
+    update("powerAmount", "Total Power: "+display(user.totPower));
+    user.blueTick=0;
+    update("blueCycle", "Reset Cycle: 0/"+user.blueMax);
   }
 }
 
@@ -62,22 +69,22 @@ function changeButtonOpacity(num) {
 }
 
 function checkUpgrade1(num) {
-  var price=upgrade1Price[num-1];
-  if(bigBigger(totPower,price)){
+  var price=user.upgrade1Price[num-1];
+  if(bigBigger(user.totPower,price)){
   //if(totPower>price){
-    totPower=bigAdd(totPower,price,0);
+    user.totPower=bigAdd(user.totPower,price,0);
     //totPower-=price;
-    update("powerAmount", "Total Power: "+display(totPower));
-    blueMults[num-1]=blueMults[num-1]+1;
+    update("powerAmount", "Total Power: "+display(user.totPower));
+    user.blueMults[num-1]=user.blueMults[num-1]+1;
     var name = "blueCircle" + num;
-    update(name, "x"+blueMults[num-1]);
+    update(name, "x"+user.blueMults[num-1]);
     price=bigMult(price,2.5,1);
     //price=Math.floor(price*2.5);
-    upgrade1Price[num-1]=price;
+    user.upgrade1Price[num-1]=price;
     price=display(price);
     update("upgrade"+num, "Upgrade your Blue Button<br/>Cost: "+price+" Power");
     var mult = 1;
-    blueMults.forEach(getMult);
+    user.blueMults.forEach(getMult);
     function getMult(value){
       mult=bigMult(mult,value,1);
       //mult*=value;
@@ -88,21 +95,42 @@ function checkUpgrade1(num) {
 }
 
 function checkAddBlue() {
-  if(bigBigger(totPower,addBluePrice)){
+  if(bigBigger(user.totPower,user.addBluePrice)){
   //if(totPower>addBluePrice){
-    totPower=bigAdd(totPower,addBluePrice,0);
+    user.totPower=bigAdd(user.totPower,user.addBluePrice,0);
     //totPower-=addBluePrice;
-    blueIndex++;
-    update("powerAmount", "Total Power: "+display(totPower));
-    document.getElementById("buttonSet"+blueIndex).style.display="block";
-    blueMults.push(1);
-    upgrade1Price.push("1e"+blueIndex);
+    user.blueIndex++;
+    update("powerAmount", "Total Power: "+display(user.totPower));
+    document.getElementById("buttonSet"+user.blueIndex).style.display="block";
+    user.blueMults.push(1);
+    user.upgrade1Price.push("1e"+user.blueIndex);
     //upgrade1Price.push(Math.pow(10,blueIndex));
-    addBluePrice=bigMult(addBluePrice,10,1);
-    var dispAddBluePrice = display(addBluePrice);
+    user.addBluePrice=bigMult(user.addBluePrice,10,1);
+    var dispAddBluePrice = display(user.addBluePrice);
     //addBluePrice*=10;
     update("addBlueButton", "Add another Blue Button<br/>Cost: "+dispAddBluePrice+" Power");
   }
+}
+
+function save(){
+	localStorage.setItem("save",JSON.stringify(user));
+}
+
+function load(){
+	var save = JSON.parse(localStorage.getItem("save"));
+	if(localStorage.getItem("save") !== null) {
+		user = convertSave(save,getDefaultSave());
+		updateSave()
+	}
+	return user;
+}
+
+function convertSave(obj,obj2) {
+	if(typeof obj === "object" && obj !== null && typeof obj2 === "object" && obj2 !== null) {
+		for(var i in obj)	obj2[i] = convertSave(obj[i],obj2[i]);
+		return obj2;
+	} 
+  else return obj;
 }
 
 function startCycle(){
