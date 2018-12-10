c209c0a231b02d3a60c1bd4862698c5bab5cab7e
Alex Henderson <bohroco@aol.com>
2018-12-10 16:45:27 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,43 +1,26 @@
-var blueTick: 0,
-var blueMax: 1000,
-var totPower: 0,
-var blueMults: [1],
-var upgrade1Price: [10],
-var addBluePrice: 100,
-var blueIndex: 1,
-var lastTick: new Date.getTime()
-
-function getDefaultSave(){
-  return{ 
-    blueTick: 0,
-    blueMax: 1000,
-    totPower: 0,
-    blueMults: [1],
-    upgrade1Price: [10],
-    addBluePrice: 100,
-    blueIndex: 1,
-    lastTick: new Date.getTime()};
-}
-
-var user = getDefaultSave();
+user = {blueTick:0,
+            blueMax:1000,
+            totPower:0,
+            blueMults: [1],
+            upgrade1Price: [10],
+            addBluePrice: 100,
+            blueIndex: 1,
+            lastTick: new Date().getTime()
+};
 
 function update(get, set) {
   document.getElementById(get).innerHTML=set;
-}
+  }
 
 function testStuff(){
-  //var num = prompt("Please enter the numbers to work with. Comma separated.");
-  alert(user);
-  var nums = num.split(",");
-  num = bigMult(nums[0],nums[1],1);
-  alert(num);
+  console.log(user.lastTick);
 }
 
 function gameCycle(){
   if(user.blueTick<1000){
     user.blueTick+=10;
     update("blueCycle", "Reset Cycle: "+user.blueTick+"/"+user.blueMax);
-    changeButtonOpacity(user.blueTick/10);
+    changeButtonOpacity(Math.max(200,user.blueTick)/10);
   }
   for(i=0;i<user.upgrade1Price.length;i++){
     if(bigBigger(user.totPower,user.upgrade1Price[i])){
@@ -48,6 +31,7 @@ function gameCycle(){
       var j = i+1;
       document.getElementById("upgrade"+j).style.opacity = 0.6;
     }
+    updateAll();
   }
   if(bigBigger(user.totPower,user.addBluePrice)) document.getElementById("addBlueButton").style.opacity = 1.0;
   else document.getElementById("addBlueButton").style.opacity = 0.6;
@@ -73,7 +57,11 @@ function blueClick() {
 function changeButtonOpacity(num) {
     var buttons = document.getElementsByClassName("blueButton");
     for(i=0; i<buttons.length; i++) {
-      buttons[i].style.opacity = num;
+      buttons[i].style.opacity = num/100;
+    }
+    var buttons2 = document.getElementsByClassName("blueButtonSmall");
+    for(i=0; i<buttons2.length; i++) {
+      buttons2[i].style.opacity = num/100;
     }
 }
 
@@ -112,36 +100,69 @@ function checkAddBlue() {
     update("powerAmount", "Total Power: "+display(user.totPower));
     document.getElementById("buttonSet"+user.blueIndex).style.display="block";
     user.blueMults.push(1);
-    user.upgrade1Price.push("1e"+user.blueIndex);
+    user.upgrade1Price.push(display("1e"+user.blueIndex));
     //upgrade1Price.push(Math.pow(10,blueIndex));
     user.addBluePrice=bigMult(user.addBluePrice,10,1);
     var dispAddBluePrice = display(user.addBluePrice);
-    //addBluePrice*=10;
     update("addBlueButton", "Add another Blue Button<br/>Cost: "+dispAddBluePrice+" Power");
   }
 }
 
 function save(){
 	localStorage.setItem("save",JSON.stringify(user));
+        console.log(JSON.stringify(user));
+	document.getElementById("savedInfo").style.display="inline";
+	function foo() {document.getElementById("savedInfo").style.display="none"}
+	setTimeout(foo, 2000);
 }
 
 function load(){
-	var save = JSON.parse(localStorage.getItem("save"));
-	if(localStorage.getItem("save") !== null) {
-		user = convertSave(save,getDefaultSave());
-		updateSave()
-	}
+	if(localStorage.getItem("save") !== null) user = JSON.parse(localStorage.getItem("save"));
 	return user;
 }
-
-function convertSave(obj,obj2) {
-	if(typeof obj === "object" && obj !== null && typeof obj2 === "object" && obj2 !== null) {
-		for(var i in obj)	obj2[i] = convertSave(obj[i],obj2[i]);
-		return obj2;
-	} 
-  else return obj;
+function updateAll(){
+	update("powerAmount", "Total Power: "+display(user.totPower));
+	var mult = 1;
+    	user.blueMults.forEach(getMult);
+    	function getMult(value){
+      		mult=bigMult(mult,value,1);
+    	}
+    	var dispMult = display(mult);
+	update("powerMultArea", "Button Mult: x"+dispMult);
+	update("blueCycle", "Reset Cycle: "+user.blueTick+"/"+user.blueMax);
+	for(var i=1;i<user.blueMults.length+1;i++){
+	  var name = "blueCircle" + i;
+    update(name, "x"+user.blueMults[i-1]);
+		var price=user.upgrade1Price[i-1];
+    price=display(price);
+    update("upgrade"+i, "Upgrade your Blue Button<br/>Cost: "+price+" Power");
+    document.getElementById("buttonSet"+i).style.display="block";	
+	}
+	for(var i=user.blueMults.length+1;i<10;i++){
+    		document.getElementById("buttonSet"+i).style.display="none";
+	}
+	var dispAddBluePrice = display(user.addBluePrice);
+    	update("addBlueButton", "Add another Blue Button<br/>Cost: "+dispAddBluePrice+" Power");
+}
+function clearSave(){
+	if(confirm("Do you really want to delete your save?\nThis cannot be undone.")){
+		user = {blueTick:0,
+            	blueMax:1000,
+            	totPower:0,
+            	blueMults: [1],
+            	upgrade1Price: [10],
+            	addBluePrice: 100,
+            	blueIndex: 1,
+            	lastTick: new Date().getTime()
+		};
+		updateAll();
+		localStorage.clear();
+	}
 }
 
 function startCycle(){
+  load();
+  updateAll();
   setInterval(gameCycle, 10);
+  setInterval(save, 30000);
 }
