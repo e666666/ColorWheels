22a8b3869c2e3ca8fdfa8d9cad9a686db5cf83f1
Nyan cat <a0939712328@gmail.com>
2019-07-08 13:46:03 +0800

    And i forgot to change some functions

--- saving.js
+++ saving.js
@@ -4,16 +4,15 @@ let playerVarName = "user" // DO NOT USE THE WORD "SAVE"
 let importDangerAlertText = "Your imported save seems to be missing some values, which means importing this save might be destructive, if you have made a backup of your current save and are sure about importing this save please press OK, if not, press cancel and the save will not be imported."
 
 function onImportError() {
-    term.echo("Error: Imported save is in invalid format, please make sure you've copied the save correctly and isn't just typing gibberish.")
+    alert("Error: Imported save is in invalid format, please make sure you've copied the save correctly and isn't just typing gibberish.")
 }
 
 function onLoadError() {
-    term.echo("I think you got your save messed up so bad we can't load it, the save have been exported automatically to your clipboard for debug purpose, please send it to the developer(Nyan cat) to see what's wrong!")
-    copyStringToClipboard(save)
+    return // I will leave this to you
 }
 
 function onImportSuccess() {
-    term.echo("Save imported successfully.")
+    console.log("Save imported successfully.")
 }
 // Only change things above to fit your game UNLESS you know what you're doing
 
